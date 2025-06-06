fee54801979f5c246ed0e31b7e6d2b90395daccd
ZapReplyApi <zapreplayapi@gmail.com>
2025-06-06 18:45:47 -0300

    Update utils.go

--- src/infrastructure/whatsapp/utils.go
+++ src/infrastructure/whatsapp/utils.go
@@ -14,45 +14,45 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
-	"go.mau.fi/whatsmeow/proto/waE2E"
+	"go.mau.fi/whatsmeow"
+	waProto "go.mau.fi/whatsmeow/proto/waE2E"
+	"go.mau.fi/whatsmeow/types"
+	"go.mau.fi/whatsmeow/types/events"
 
 	"github.com/aldinokemal/go-whatsapp-web-multidevice/config"
 	pkgError "github.com/aldinokemal/go-whatsapp-web-multidevice/pkg/error"
-	"go.mau.fi/whatsmeow"
-	"go.mau.fi/whatsmeow/types"
-	"go.mau.fi/whatsmeow/types/events"
 )
 
-// ExtractMedia is a helper function to extract media from whatsapp
-func ExtractMedia(ctx context.Context, storageLocation string, mediaFile whatsmeow.DownloadableMessage) (extractedMedia ExtractedMedia, err error) {
+func ExtractMedia(ctx context.Context, storageLocation string, mediaFile whatsmeow.DownloadableMessage) (ExtractedMedia, error) {
+	var extractedMedia ExtractedMedia
 	if mediaFile == nil {
 		logrus.Info("Skip download because data is nil")
 		return extractedMedia, nil
 	}
 
-	data, err := cli.Download(ctx, mediaFile)
+	waCli := GetWaCli()
+	data, err := waCli.Download(ctx, mediaFile)
 	if err != nil {
 		return extractedMedia, err
 	}
 
-	// Validate file size before writing to disk
 	maxFileSize := config.WhatsappSettingMaxDownloadSize
 	if int64(len(data)) > maxFileSize {
 		return extractedMedia, fmt.Errorf("file size exceeds the maximum limit of %d bytes", maxFileSize)
 	}
 
 	switch media := mediaFile.(type) {
-	case *waE2E.ImageMessage:
+	case *waProto.ImageMessage:
 		extractedMedia.MimeType = media.GetMimetype()
 		extractedMedia.Caption = media.GetCaption()
-	case *waE2E.AudioMessage:
+	case *waProto.AudioMessage:
 		extractedMedia.MimeType = media.GetMimetype()
-	case *waE2E.VideoMessage:
+	case *waProto.VideoMessage:
 		extractedMedia.MimeType = media.GetMimetype()
 		extractedMedia.Caption = media.GetCaption()
-	case *waE2E.StickerMessage:
+	case *waProto.StickerMessage:
 		extractedMedia.MimeType = media.GetMimetype()
-	case *waE2E.DocumentMessage:
+	case *waProto.DocumentMessage:
 		extractedMedia.MimeType = media.GetMimetype()
 		extractedMedia.Caption = media.GetCaption()
 	}
@@ -127,49 +127,42 @@ func ParseJID(arg string) (types.JID, error) {
 
 	recipient, err := types.ParseJID(arg)
 	if err != nil {
-		fmt.Printf("invalid JID %s: %v", arg, err)
 		return recipient, pkgError.ErrInvalidJID
 	}
 
 	if recipient.User == "" {
-		fmt.Printf("invalid JID %v: no server specified", arg)
 		return recipient, pkgError.ErrInvalidJID
 	}
 	return recipient, nil
 }
 
 func IsOnWhatsapp(waCli *whatsmeow.Client, jid string) bool {
-	// only check if the jid a user with @s.whatsapp.net
 	if strings.Contains(jid, "@s.whatsapp.net") {
 		data, err := waCli.IsOnWhatsApp([]string{jid})
 		if err != nil {
-			logrus.Error("Failed to check if user is on whatsapp: ", err)
+			logrus.Error("Failed to check if user is on WhatsApp: ", err)
 			return false
 		}
-
 		for _, v := range data {
 			if !v.IsIn {
 				return false
 			}
 		}
 	}
-
 	return true
 }
 
 func ValidateJidWithLogin(waCli *whatsmeow.Client, jid string) (types.JID, error) {
 	MustLogin(waCli)
-
 	if config.WhatsappAccountValidation && !IsOnWhatsapp(waCli, jid) {
-		return types.JID{}, pkgError.InvalidJID(fmt.Sprintf("Phone %s is not on whatsapp", jid))
+		return types.JID{}, pkgError.InvalidJID(fmt.Sprintf("Phone %s is not on WhatsApp", jid))
 	}
-
 	return ParseJID(jid)
 }
 
 func MustLogin(waCli *whatsmeow.Client) {
 	if waCli == nil {
-		panic(pkgError.InternalServerError("Whatsapp client is not initialized"))
+		panic(pkgError.InternalServerError("WhatsApp client is not initialized"))
 	}
 	if !waCli.IsConnected() {
 		panic(pkgError.ErrNotConnected)
@@ -179,7 +172,6 @@ func MustLogin(waCli *whatsmeow.Client) {
 }
 
 func FormatJID(jid string) types.JID {
-	// Remove any :number suffix if present
 	if idx := strings.LastIndex(jid, ":"); idx != -1 && strings.Contains(jid, "@s.whatsapp.net") {
 		jid = jid[:idx] + jid[strings.Index(jid, "@s.whatsapp.net"):]
 	}
@@ -190,26 +182,21 @@ func FormatJID(jid string) types.JID {
 	return formattedJID
 }
 
-// isGroupJid is a helper function to check if the message is from group
 func isGroupJid(jid string) bool {
 	return strings.Contains(jid, "@g.us")
 }
 
-// isFromMySelf is a helper function to check if the message is from my self (logged in account)
 func isFromMySelf(jid string) bool {
-	return extractPhoneNumber(jid) == extractPhoneNumber(cli.Store.ID.String())
+	waCli := GetWaCli()
+	return extractPhoneNumber(jid) == extractPhoneNumber(waCli.Store.ID.String())
 }
 
-// extractPhoneNumber is a helper function to extract the phone number from a JID
 func extractPhoneNumber(jid string) string {
 	regex := regexp.MustCompile(`\d+`)
-	// Find all matches of the pattern in the JID
 	matches := regex.FindAllString(jid, -1)
-	// The first match should be the phone number
 	if len(matches) > 0 {
 		return matches[0]
 	}
-	// If no matches are found, return an empty string
 	return ""
 }
 
@@ -220,48 +207,6 @@ func getMessageDigestOrSignature(msg, key []byte) (string, error) {
 		return "", err
 	}
 	return hex.EncodeToString(mac.Sum(nil)), nil
-}
-
-func buildEventMessage(evt *events.Message) (message evtMessage) {
-	message.Text = evt.Message.GetConversation()
-	message.ID = evt.Info.ID
-
-	if extendedMessage := evt.Message.GetExtendedTextMessage(); extendedMessage != nil {
-		message.Text = extendedMessage.GetText()
-		message.RepliedId = extendedMessage.ContextInfo.GetStanzaID()
-		message.QuotedMessage = extendedMessage.ContextInfo.GetQuotedMessage().GetConversation()
-	} else if protocolMessage := evt.Message.GetProtocolMessage(); protocolMessage != nil {
-		if editedMessage := protocolMessage.GetEditedMessage(); editedMessage != nil {
-			if extendedText := editedMessage.GetExtendedTextMessage(); extendedText != nil {
-				message.Text = extendedText.GetText()
-				message.RepliedId = extendedText.ContextInfo.GetStanzaID()
-				message.QuotedMessage = extendedText.ContextInfo.GetQuotedMessage().GetConversation()
-			}
-		}
-	}
-
-	return message
-}
-
-func buildEventReaction(evt *events.Message) (waReaction evtReaction) {
-	if reactionMessage := evt.Message.GetReactionMessage(); reactionMessage != nil {
-		waReaction.Message = reactionMessage.GetText()
-		waReaction.ID = reactionMessage.GetKey().GetID()
-	}
-	return waReaction
-}
-
-func buildForwarded(evt *events.Message) bool {
-	if extendedText := evt.Message.GetExtendedTextMessage(); extendedText != nil {
-		return extendedText.ContextInfo.GetIsForwarded()
-	} else if protocolMessage := evt.Message.GetProtocolMessage(); protocolMessage != nil {
-		if editedMessage := protocolMessage.GetEditedMessage(); editedMessage != nil {
-			if extendedText := editedMessage.GetExtendedTextMessage(); extendedText != nil {
-				return extendedText.ContextInfo.GetIsForwarded()
-			}
-		}
-	}
-	return false
 }
 
 func ExtractMessageText(evt *events.Message) string {
